941d41628db8b041bcd990ceb1b3442a032eb7e4
borbware <perttu.tuovinen@gmail.com>
2022-05-04 13:21:19 +0300

    add more class stuff

--- Assets/ClassScripting.cs
+++ Assets/ClassScripting.cs
@@ -1,12 +1,27 @@
 using UnityEngine;
+using ClassExample;
 
 public class ClassScripting : MonoBehaviour
 {
-    void Start()
+    void BasicExamples()
     {
         Debug.Log(Enemy.enemyDescription);
-        var enemy1 = new Enemy();
-        var classExample = new ClassExample();
+        var enemy1 = new Enemy(10, "Vihu");
+        var enemy2 = new Enemy();
+        var classExample = new ExampleClass();
         classExample.createEnemies();
+    }
+    void Start()
+    {
+        // BasicExamples();
+
+        var grunt1 = new Grunt(1);
+        var grunt2 = new Grunt(2);
+        var grunt3 = new Grunt(3);
+        Enemy.DescribeEnemies();
+        Grunt.DescribeEnemies();
+
+        grunt1.SetHP(2);
+
     }
 }
--- Assets/Scripts/ClassExample.cs
+++ Assets/Scripts/ClassExample.cs
@@ -1,29 +1,66 @@
 using UnityEngine;
 
-public class ClassExample
+namespace ClassExample
 {
-    public void createEnemies()
+    public class ExampleClass
     {
-        var enemy1 = new Enemy();
-        enemy1.SetHP(4);
+        public void createEnemies()
+        {
+            var enemy1 = new Enemy(100, "toinen vihu");
+            enemy1.SetHP(4);
 
-        Enemy.DescribeEnemies();
-        Debug.Log(Enemy.enemyDescription);
+            Enemy.DescribeEnemies();
+            Debug.Log(Enemy.enemyDescription);
+        }
     }
-}
 
-public class Enemy
-{
-    private int HP = 0;
-    public static string enemyDescription = "Enemies are bouncy";
-    public void SetHP(int newHP)
+    public class Enemy
     {
-        Debug.Log(enemyDescription);
-        this.HP = newHP;
+        protected int HP = 0;
+        protected int maxHP = 0;
+        protected string name; 
+        public static string enemyDescription = "Enemies are bouncy";
+
+        public Enemy(int newMaxHP, string newName)
+        {
+            maxHP = newMaxHP;
+            HP = maxHP;
+            name = newName;
+            Debug.Log($"Enemy {newName} created with HP {HP}!");
+        }
+        public Enemy()
+        {
+            maxHP = 10;
+            HP = maxHP;
+            name = "DefaulttiNimi";
+            Debug.Log($"Enemy {name} created with HP {HP}!");
+        }
+        public virtual void SetHP(int newHP)
+        {
+            Debug.Log(enemyDescription);
+            this.HP = newHP;
+        }
+        public static void DescribeEnemies()
+        {
+            Debug.Log("Enemies are spiky");
+        }
     }
-    public static void DescribeEnemies()
+
+    public class Grunt : Enemy
     {
-        Debug.Log("Enemies are spiky");
-        Debug.Log(enemyDescription);
+        public Grunt(int index)
+        {
+            name = "Grunt " + index;
+            maxHP = 5;
+            HP = maxHP;
+        }
+        public void SetHP(int newHP)
+        {
+            this.HP = newHP + 1;
+        }
+        public static new void DescribeEnemies()
+        {
+            Debug.Log("Grunts are silly");
+        }
     }
 }
